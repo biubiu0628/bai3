f2815593192516a28a0a6b0639215b91f690bd8d
biubiu0628 <pewpew0628@gmail.com>
2024-11-25 12:55:37 +0700

    Add project files.

---
+++ bai3/Controllers/Student.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bai3.Controllers
+{
+    public class Student : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ShowKQ(int id, string name, string nganh) {
+            return Content(string.Format("Cam on ban {0}-{1} da dang ky chuyen nganh {2}.", id, name, nganh));
+        }
+    }
+}
